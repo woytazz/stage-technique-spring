a399f6d187cd6531bdecaf11c3cd6126849be1ad
230040 Wojtasik Adrian <230040@edu.p.lodz.pl>
2022-07-11 16:27:53 +0200

    Initial commit

---
+++ src/main/java/pl/technique/stage/StageTechniqueApplication.java
@@ -0,0 +1,13 @@
+package pl.technique.stage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StageTechniqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StageTechniqueApplication.class, args);
+	}
+
+}
